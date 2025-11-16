d55814f022e36338f1f205b0d909c8c6813d61ee
Kanhaiya Mittal <kanhaiyamittal8755@gmail.com>
2025-11-16 23:33:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,64 +8,60 @@ const homeRouter = require("./routes/home");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
 const bodyParser = require('body-parser');
-
 const users = require("./models/users");
 
 const url = "mongodb+srv://kanhaiyamittal26_db_user:%23Kmittal8445@codelive.awtcsgh.mongodb.net/code_editor?retryWrites=true&w=majority&appName=CodeLive";
+
 const dbName = "code_editor";
+// const dbUrl = url + dbName;
 const port = process.env.PORT || 8080;
-const expire_duration = 7 * 60 * 60 * 1000; // 7 hours
+const expire_duration = 7 * 60 * 60 * 1000;
 
-async function main() {
-  try {
-    await mongoose.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log("Connected to " + dbName);
-    
-    var store = new MongoDBStore({
-      uri: url,
-      collection: "mySessions",
-    });
+main().catch((err) => console.log(err));
 
-    store.on("error", function (error) {
-      console.log(error);
-    });
+async function main()
+{
+  app.listen(port, (err) => {
+    if (err) throw err;
+    console.log("Listening on " + port);
+  });
 
-    app.use(
-      session({
-        secret: "secert/key for signing cookie",
-        resave: false,
-        saveUninitialized: false,
-        cookie: { maxAge: expire_duration },
-        store: store,
-      })
-    );
+  var store = new MongoDBStore({
+    uri: url,
+    collection: "mySessions",
+  });
 
+  store.on("error", function (error) {
+    console.log(error);
+  });
 
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(express.static(__dirname + "/public"));
-    app.set("view engine", "ejs");
+  app.use(
+    session({
+      secret: "secert/key for signing cookie",
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: expire_duration },
+      store: store,
+    })
+  );
 
-    app.use("/login", loginRouter);
-    app.use("/signup", signupRouter);
-    app.use("/home", homeRouter);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended: false}));
 
-    // Root redirect
-    app.get("/", (req, res) => {
-      res.redirect("/home");
-    });
+  app.use(express.static(__dirname + "/public"));
+  app.set("view engine", "ejs");
+  app.use("/login", loginRouter);
+  app.use("/signup", signupRouter);
+  app.use("/home", homeRouter);
+  //root
+  app.get("/", (req, res) => {
+    res.redirect('/home');
+  });
 
-    app.listen(port, () => {
-      console.log("Listening on port " + port);
-    });
-
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1); // Exit if DB connection fails
-  }
+  mongoose.connect(url);
+  const con = mongoose.connection;
+  con.on("error", (err) => {
+    throw err;
+  });
+  con.once("open", () => console.log("Connected to " + dbName));
 }
-
-main();
